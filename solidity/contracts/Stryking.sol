9ee7fd2404cb41a1a08baa002b038cfad9587484
lentan <lentan1029@gmail.com>
2018-05-27 21:04:26 +0800

    Add stryking contract

--- solidity/contracts/Stryking.sol
+++ solidity/contracts/Stryking.sol
@@ -12,6 +12,81 @@ interface IERC20 {
 }
 
 
+/**
+ * @title Eliptic curve signature operations
+ *
+ * @dev Based on https://gist.github.com/axic/5b33912c6f61ae6fd96d6c4a47afde6d
+ *
+ * TODO Remove this library once solidity supports passing a signature to ecrecover.
+ * See https://github.com/ethereum/solidity/issues/864
+ *
+ */
+
+library ECRecovery {
+
+  /**
+   * @dev Recover signer address from a message by using their signature
+   * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
+   * @param sig bytes signature, the signature is generated using web3.eth.sign()
+   */
+  function recover(bytes32 hash, bytes sig)
+    internal
+    pure
+    returns (address)
+  {
+    bytes32 r;
+    bytes32 s;
+    uint8 v;
+
+    // Check the signature length
+    if (sig.length != 65) {
+      return (address(0));
+    }
+
+    // Divide the signature in r, s and v variables
+    // ecrecover takes the signature parameters, and the only way to get them
+    // currently is to use assembly.
+    // solium-disable-next-line security/no-inline-assembly
+    assembly {
+      r := mload(add(sig, 32))
+      s := mload(add(sig, 64))
+      v := byte(0, mload(add(sig, 96)))
+    }
+
+    // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
+    if (v < 27) {
+      v += 27;
+    }
+
+    // If the version is correct return the signer address
+    if (v != 27 && v != 28) {
+      return (address(0));
+    } else {
+      // solium-disable-next-line arg-overflow
+      return ecrecover(hash, v, r, s);
+    }
+  }
+
+  /**
+   * toEthSignedMessageHash
+   * @dev prefix a bytes32 value with "\x19Ethereum Signed Message:"
+   * @dev and hash the result
+   */
+  function toEthSignedMessageHash(bytes32 hash)
+    internal
+    pure
+    returns (bytes32)
+  {
+    // 32 is the length in bytes of hash,
+    // enforced by the type signature above
+    return keccak256(
+      "\x19Ethereum Signed Message:\n32",
+      hash
+    );
+  }
+}
+
+
 library SafeMath {
   function mul(uint256 a, uint256 b) internal pure returns (uint256) {
     if (a == 0) {
@@ -42,8 +117,61 @@ library SafeMath {
 }
 
 
-contract Stryking is IERC20 {
+/**
+ * @title Ownable
+ * @dev The Ownable contract has an owner address, and provides basic authorization control
+ * functions, this simplifies the implementation of "user permissions".
+ */
+contract Ownable {
+  address public owner;
+
+
+  event OwnershipRenounced(address indexed previousOwner);
+  event OwnershipTransferred(
+    address indexed previousOwner,
+    address indexed newOwner
+  );
+
+
+  /**
+   * @dev The Ownable constructor sets the original `owner` of the contract to the sender
+   * account.
+   */
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  /**
+   * @dev Allows the current owner to transfer control of the contract to a newOwner.
+   * @param newOwner The address to transfer ownership to.
+   */
+  function transferOwnership(address newOwner) public onlyOwner {
+    require(newOwner != address(0));
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+
+  /**
+   * @dev Allows the current owner to relinquish control of the contract.
+   */
+  function renounceOwnership() public onlyOwner {
+    emit OwnershipRenounced(owner);
+    owner = address(0);
+  }
+}
+
+
+contract Stryking is IERC20, Ownable {
   using SafeMath for uint256;
+  using ECRecovery for bytes32;
 
   string public name = "Stryking Token";
   string public symbol = "STRRRRYZ";
@@ -52,6 +180,7 @@ contract Stryking is IERC20 {
   uint256 public constant totalSupply = 2000000000 * decimalFactor;
   mapping (address => uint256) public balances;
   mapping (address => mapping (address => uint256)) internal allowed;
+  mapping (address => mapping (address => uint256)) internal specialAllowed;
   event Transfer(address indexed from, address indexed to, uint256 value);
   event Approval(address indexed owner, address indexed spender, uint256 value);
 
@@ -60,12 +189,25 @@ contract Stryking is IERC20 {
     emit Transfer(address(0), msg.sender, totalSupply);
   }
 
+  function toBytes(address a) public pure returns (bytes b){
+    assembly {
+          let m := mload(0x40)
+          mstore(add(m, 20), xor(0x140000000000000000000000000000000000000000, a))
+          mstore(0x40, add(m, 52))
+          b := m
+    }
+  }
+
   function balanceOf(address _owner) public view returns (uint256 balance) {
     return balances[_owner];
   }
 
   function allowance(address _owner, address _spender) public view returns (uint256) {
     return allowed[_owner][_spender];
+  }
+
+  function specialAllowance(address _owner, address _spender) public view returns (bool) {
+    return specialAllowed[_owner][_spender] % 2 == 1;
   }
 
   function transfer(address _to, uint256 _value) public returns (bool) {
@@ -82,11 +224,12 @@ contract Stryking is IERC20 {
   function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
     require(_to != address(0));
     require(_value <= balances[_from]);
-    require(_value <= allowed[_from][msg.sender]);
-
+    require(_value <= allowed[_from][msg.sender] || specialAllowed[_from][msg.sender] % 2 == 1);
     balances[_from] = balances[_from].sub(_value);
     balances[_to] = balances[_to].add(_value);
-    allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    if (specialAllowed[_from][msg.sender] % 2 == 0) {
+      allowed[_from][msg.sender] = allowed[_from][msg.sender].sub(_value);
+    }
     emit Transfer(_from, _to, _value);
     return true;
   }
@@ -94,6 +237,14 @@ contract Stryking is IERC20 {
   function approve(address _spender, uint256 _value) public returns (bool) {
     allowed[msg.sender][_spender] = _value;
     emit Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function specialApprove(uint256 _nonce, bytes32 _ethSignedMessageHash, bytes sig) public returns (bool) {
+    address owner = _ethSignedMessageHash.recover(sig);
+    require(_nonce == specialAllowed[owner][msg.sender] + 1);
+    require(keccak256(_nonce).toEthSignedMessageHash() == _ethSignedMessageHash);
+    specialAllowed[owner][msg.sender] = specialAllowed[owner][msg.sender] + 1;
     return true;
   }
 
