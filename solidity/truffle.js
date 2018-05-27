916a1ad1cf68da50019ad13b5fda37eaeb64c75e
lentan <lentan1029@gmail.com>
2018-05-27 15:27:27 +0800

    Add truffle

---
+++ solidity/truffle.js
@@ -0,0 +1,33 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const private = require('../private.json');
+const mnemonic = private.funds;
+const infuraKey = private.infura;
+
+module.exports = {
+  networks: {
+   development: {
+      host: 'localhost',
+      port: 7545,
+      network_id: '*', // Match any network id
+      gas: 3500000,
+    },
+   ropsten: {
+      provider: new HDWalletProvider(mnemonic, "https://ropsten.infura.io/4cQUeyeUSfkCXsgEAUH2"),
+      network_id: '*',
+      gas: 3500000,
+      gasPrice: 50000000000, // 50 gwei, this is very high
+    },
+    rinkeby: {
+       provider: new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/4cQUeyeUSfkCXsgEAUH2"),
+       network_id: '*',
+       gas: 3500000,
+       gasPrice: 5000000000, // 50 gwei, this is very high
+     },
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200,
+    },
+  },
+};
