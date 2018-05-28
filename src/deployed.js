13f59d48062b6bcb149617ee657bed6e0720be27
lentan <lentan1029@gmail.com>
2018-05-28 22:22:49 +0800

    Add missing lines

---
+++ src/deployed.js
@@ -0,0 +1,3 @@
+module.exports = {
+  stryking: "0xf08e0d0cde5aaceb3be97476b3979036cfbf3923"
+}
--- src/web3.js
+++ src/web3.js
@@ -2,4 +2,5 @@ const Web3 = require('web3');
 const private = require('../private.json');
 const web3 = new Web3(new Web3.providers.HttpProvider(`https://ropsten.infura.io/${private.infura}`));
 const abi = require('./abi.js')
-const contract = web3.eth.contract(abi.stryking).at
+const deployed = require('./deployed.js');
+const contract = web3.eth.contract(abi.stryking).at(deployed.stryking);
