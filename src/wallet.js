5aec5658e56b9c8d90552e980a5b6432f4873565
lentan <tetratorus@gmail.com>
2018-06-23 10:35:44 +0800

    Fix issues with wallet to keep derivation consistent with Metamask

--- src/wallet.js
+++ src/wallet.js
@@ -1,28 +1,29 @@
 const ethUtils = require('ethereumjs-util');
-const wallet = require('ethereumjs-wallet');
 const hdkey = require('ethereumjs-wallet/hdkey');
 const private = require('../private.json');
-const seed = private.seed;
-const fundsWalletSeed = private.funds;
+const bip39 = require('bip39');
 
 module.exports = (function(){
 
   const _private = {
-    hdWallet: hdkey.fromMasterSeed(seed),
-    fundsWallet: hdkey.fromMasterSeed(private.funds)
-  }
+    getNodePath: (index) => {
+      return `m/44'/60'/0'/0/${index}`;
+    },
+    hdWallet: hdkey.fromMasterSeed(bip39.mnemonicToSeed(private.seed)),
+    fundsWallet: hdkey.fromMasterSeed(bip39.mnemonicToSeed(private.funds))
+  };
 
   const _public = {
     hdWallet: _private.hdWallet,
-    getChildWallet: (index) => _private.hdWallet.deriveChild(index).getWallet(),
-    getFundsWallet: () => _private.fundsWallet.deriveChild(0).getWallet(),
+    getChildWallet: (index) => _private.hdWallet.derivePath(_private.getNodePath(index)).getWallet(),
+    getFundsWallet: () => _private.fundsWallet.derivePath(_private.getNodePath(0)).getWallet(),
     parseWallet: (wallet) => {
       const privateKey = wallet.getPrivateKey().toString('hex');
       const publicKey = wallet.getPublicKey().toString('hex');
       const address = ethUtils.pubToAddress(wallet.getPublicKey()).toString('hex');
       return { privateKey, publicKey, address };
     }
-  }
+  };
 
   return _public;
 })();
